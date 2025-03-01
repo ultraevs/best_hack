19942a68d6f2f94e49172c53390228c68b8b0b87
Mikhail <evstabpro@gmail.com>
2025-03-02 01:46:51 +0300

    admin route

--- python-backend/app/main.py
+++ python-backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.api.v1.endpoints import lots, orders, auth, user, filters
+from app.api.v1.endpoints import lots, orders, auth, user, filters, admin
 from app.db.session import engine
 from app.db.models.base import Base
 from fastapi.middleware.cors import CORSMiddleware
@@ -26,3 +26,4 @@ app.include_router(orders.router, prefix="/api/v1")
 app.include_router(auth.router, prefix="/api/v1")
 app.include_router(user.router, prefix="/api/v1")
 app.include_router(filters.router, prefix="/api/v1")
+app.include_router(admin.router, prefix="/api/v1")
