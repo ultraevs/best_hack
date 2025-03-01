fbbf5523ad75542635dd86b4947400f22439d318
Mikhail <evstabpro@gmail.com>
2025-03-01 18:06:21 +0300

    cors

--- python-backend/app/main.py
+++ python-backend/app/main.py
@@ -2,10 +2,18 @@ from fastapi import FastAPI
 from app.api.v1.endpoints import lots, orders, auth
 from app.db.session import engine
 from app.db.models.base import Base
-
+from fastapi.middleware.cors import CORSMiddleware
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(lots.router, prefix="/api/v1")
 app.include_router(orders.router, prefix="/api/v1")
