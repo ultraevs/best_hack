7601c124fd1a4bf720fc2761590684dac075d486
Konstantin <kostya1703tsoy@gmail.com>
2025-03-02 07:25:47 +0300

    update NotFound Page

--- app/src/pages/NotFound/NotFound.tsx
+++ app/src/pages/NotFound/NotFound.tsx
@@ -1,3 +1,21 @@
+import { routes } from '@/router/routes'
+import { Button, Flex } from 'antd'
+import { useNavigate } from 'react-router'
+import styled from 'styled-components'
+
+const Content = styled(Flex)`
+  height: calc(100vh - 96px);
+`
+
 export function NotFound() {
-  return <h1>NotFound Page</h1>
+  const navigate = useNavigate()
+
+  return (
+    <Content align='center' vertical flex={1} justify='center' gap={16}>
+      <h1>Страница не найдена</h1>
+      <Button onClick={() => navigate(routes.main.url)}>
+        Вернуться на главную
+      </Button>
+    </Content>
+  )
 }
