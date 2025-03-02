a6987440c83cc00d7796a74abdc0889ff56cee0c
Mikhail <evstabpro@gmail.com>
2025-03-02 08:19:12 +0300

    fuck

--- python-backend/app/main.py
+++ python-backend/app/main.py
@@ -15,7 +15,7 @@ app = FastAPI(
         "name": "MIT",
     },
     docs_url="/api/v1/docs",
-    openapi_url="/api/v1/docs/openapi.json"
+    openapi_url="/api/v1/docs/openapi.json",
     swagger_ui_parameters={"syntaxHighlight.theme": "monokai", "theme": "dark",}
 )
 @app.on_event("startup")
